12065540e99f49825dad517e6d5b6fc1c65ebcd7
Blake Simas <blakesimas@gmail.com>
2017-06-16 10:52:06 -0600

    Updated styling and task model

--- mysite/to_do/models.py
+++ mysite/to_do/models.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 # Create your models here.
 class Task(models.Model):
     name = models.CharField(max_length=264)
-    description = models.TextField()
+    description = models.CharField(max_length=500)
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
